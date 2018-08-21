7a062727fce75aca33d92e4c471dc587985fb2ed
Igor Kramar <hombrehumor@gmail.com>
2018-08-22 00:39:34 +0600

    add slider, modal and form scripts

--- build/js/common.js
+++ build/js/common.js
@@ -32,6 +32,78 @@ function mobilemenu(btn, closeBtn, menuItem, menu) {
   });
 }
 
+function carousel(container, prevBtn, nextBtn) {
+  var carousel = $(container).owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: false,
+    dots: false,
+    items: 1
+  });
+
+  $(nextBtn).click(function () {
+    carousel.trigger('next.owl.carousel');
+  });
+
+  $(prevBtn).click(function () {
+    carousel.trigger('prev.owl.carousel');
+  });
+}
+
+function form() {
+  $('#section-form__form').on('submit', function (event) {
+    event.preventDefault();
+
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://webdev-api.loftschool.com/sendmail",
+      "method": "POST",
+      "headers": {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      "data": {
+        "name": $('.section-form__input[name="name"]').val(),
+        "phone": $('.section-form__input[name="phone"]').val(),
+        "comment": $('.section-form__input[name="comment"]').val(),
+        "to": "hombrehumor@gmail.com"
+      }
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+  });
+}
+
+function modal(params) {
+  $(params.openBtn).on('click', function () {
+    var inTitle = $(this).parent().parent().find(params.title).text();
+    var inText = $(this).parent().parent().find(params.text).text();
+    $(params.modal).find(params.modalTitle).text(inTitle);
+    $(params.modal).find(params.modalText).text(inText);
+    $(params.overlay).fadeIn();
+    $(params.modal).fadeIn();
+
+    $(params.closeBtn).on('click', function () {
+      $(params.overlay).fadeOut();
+      $(params.modal).fadeOut();
+    });
+  });
+}
+
 accordeon('.section-team__btn');
 accordeon('.section-menu__button');
 mobilemenu('.hamburger-button', '.mobile__close', '.mobile-nav__link', '.mobile');
+carousel('.owl-carousel', '.slider-left__btn', '.slider-right__btn');
+form();
+modal({
+  openBtn: '.review__view',
+  title: '.review__title',
+  text: '.review__text',
+  modal: '.modal',
+  overlay: '.overlay',
+  modalTitle: '.modal__title',
+  modalText: '.modal__text',
+  closeBtn: '.modal__close'
+});
--- gulp/paths/css.vendor.js
+++ gulp/paths/css.vendor.js
@@ -1,5 +1,7 @@
-"use strcit";
+"use strict";
 
 module.exports = [
-  './node_modules/normalize.css/normalize.css'
+  './node_modules/normalize.css/normalize.css',
+  './source/style/owl/owl.carousel.min.css',
+  './source/style/owl/owl.theme.default.min.css'
 ];
